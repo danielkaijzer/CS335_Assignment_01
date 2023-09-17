c53a0e088cc4c9faa56d14c61ae2876358a6d2bc
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-17 16:03:07 -0400

    Setup for Card class files

---
+++ Card.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file Card.cpp
+ * @author Daniel Kaijzer
+ * @brief Implementation file for "Card" class
+ * @version 0.1
+ * @date 2023-09-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "Card.hpp"
+
+Card::Card(){
+    
+} // default constructor
---
+++ Card.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file Card.hpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef CARD_
+#define CARD_
+
+class Card{
+    private:
+    public:
+        Card(); // default constructor
+};
+
+#include "Card.cpp"
+
+#endif
