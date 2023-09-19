c272eb3d981d615ed7d443db2bb06bb5b6212a58
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-19 18:19:07 -0400

    Setup

---
+++ CardGame.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file CardGame.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "CardGame.hpp"
+
+CardGame::CardGame(){
+
+}
---
+++ CardGame.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file CardGame.hpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef CARD_GAME_
+#define CARD_GAME_
+
+class CardGame{
+    public:
+        CardGame(); // Default Constructor
+};
+
+#include "CardGame.cpp"
+
+#endif
