35448ce8d411ef4ebf3bed71a76d52498db253f5
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-18 23:01:21 -0400

    Setup

---
+++ Hand.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file Hand.cpp
+ * @author Daniel Kaijzer
+ * @brief implementation file for Hand class
+ * @version 0.1
+ * @date 2023-09-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "Hand.hpp"
+
+Hand::Hand(){
+
+}
---
+++ Hand.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file Hand.hpp
+ * @author Daniel Kaijzer
+ * @brief interface file for Hand class
+ * @version 0.1
+ * @date 2023-09-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef HAND_
+#define HAND_
+
+class Hand{
+    private:
+    public:
+        Hand(); // default constructor
+};
+
+#include "Hand.cpp"
+
+#endif
