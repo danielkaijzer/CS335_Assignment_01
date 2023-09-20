55685445920d5feff2aa02f61328c36d0a0679df
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-20 19:08:17 -0400

    Added methods and members

--- Card.cpp
+++ Card.cpp
@@ -11,9 +11,49 @@
 
 #include "Card.hpp"
 
-Card::Card(){
+// Copy Constructor
+Card::Card(const Card & rhs){
+    type_ = new std::string{*rhs.type_};
+    text_ = new std::string{*rhs.text_};
+    image_ = new std::vector<int>{*rhs.image_};
+}
 
-} // default constructor
+// Copy Assignment Operator
+Card & Card::operator=(const Card & rhs){
+    if (this != &rhs){
+        *type_ = *rhs.type_;
+        *text_ = *rhs.text_;
+        *image_ = *rhs.image_;
+    }
+    return *this;
+}
+
+// Move Constructor
+Card::Card(Card && rhs){
+    type_ = rhs.type_;
+    delete rhs.type_;
+    rhs.type_ = nullptr;
+
+    text_ = rhs.text_;
+    delete rhs.text_;
+    rhs.text_ = nullptr; 
+
+    image_ = std::move(rhs.image_);
+}
+
+// Move Assignment Operator
+Card & Card::operator=(Card && rhs){
+    // Use std::swap for all data members
+    std::swap(type_,rhs.type_);
+    std::swap(text_, rhs.text_);
+    std::swap(image_, rhs.image_);
+    return *this;
+}
+
+// Destructor
+Card::~Card(){
+    // delete type_, text_, image_;
+}
 
 void Card::Print(){
     std::cout << "Hello world" << std::endl;
--- Card.hpp
+++ Card.hpp
@@ -13,11 +13,36 @@
 #define CARD_
 
 #include <iostream>
+#include <vector>
+
 
 class Card{
     private:
+        std::string *type_;
+        std::string *text_;
+        std::vector<int> *image_;
+
     public:
-        Card(); // default constructor
+        Card() = default; // default constructor
+
+        // BIG 5
+
+        // Copy Constructor
+        Card(const Card & rhs);
+
+        // Copy Assignment Operator
+        Card & operator=(const Card & rhs);
+
+        // Move Constructor
+        Card(Card && rhs);
+
+        // Move Assignment Operator
+        Card & operator=(Card && rhs);
+
+        // Destructor
+        ~Card();
+
+        // virtual void Play() = 0;
 
         void Print(); // Test method
 };
