23a1cb6bfc50192c01e43aa37a62e3720d487b07
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-21 23:05:31 -0400

    Setup

---
+++ ActionCard.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file ActionCard.hpp
+ * @author Daniel Kaijzer
+ * @brief ActionCard class, derived from Card class. Interface file.
+ * @version 0.1
+ * @date 2023-09-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef ACTION_CARD_
+#define ACTION_CARD_
+
+#include "Card.hpp"
+
+class ActionCard : public Card {
+    ActionCard();
+
+};
+
+#include "ActionCard.cpp"
+
+#endif ACTION_CARD_
